96d00a727002cd409b36492c7505c8488af2b160
sandun-win10 <sandun-win10@192.168.100.104>
2020-05-04 13:49:37 -0500

    first commit

---
+++ mygit4/src/main/java/com/cucmber/mygit4/mygit4/App.java
@@ -0,0 +1,13 @@
+package com.cucmber.mygit4.mygit4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
